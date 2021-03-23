b6ae9a6f37aa251f26b1b5e18d3c8a41318f2880
Yann Martin <ymartin@la-providence.net>
2021-03-23 16:10:13 +0100

    modif

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-<?php echo "Covid"; ?>
+<?php
+
+$db = new PDO("mysql:host=localhost;dbname=yannmartin_covid;", "229066", "yann1234");
+
+if(isset($_POST["form_login"])) {
+    $reqUser = $db->prepare("SELECT * FROM user WHERE email = ? AND password = ?");
+    $reqUser->execute(array($_POST["email"], $_POST["password"]));
+    $countUser = $reqUser->rowCount();
+
+    if($countUser > 0) {
+        $user = $reqUser->fetch();
+        echo "ok";
+    }
+}
+
+?>
+<html>  
+<head>
+    <title>Connexion</title>
+
+    <form method="POST">
+    <input type="text" name="email" />
+    <input type="password" name="password" />
+    <input type="submit" name="form_login" />
+    </form>
+</head>
+<body>
+</body>
+</html>
